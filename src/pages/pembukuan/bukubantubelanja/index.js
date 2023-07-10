a27820c2589959b69f852f81fd85ab1613f3b6c2
haris28 <harisibnu@28gmail.com>
2023-07-10 08:43:23 +0700

    update

--- src/pages/pembukuan/bukubantubelanja/index.js
+++ src/pages/pembukuan/bukubantubelanja/index.js
@@ -5,14 +5,11 @@ import {
   Select,
   DatePicker,
   Divider,
-  Space,
-  Button,
-  Tag,
   notification,
   Spin,
 } from "antd";
 import React, { useEffect, useState } from "react";
-import { Buttons, DatePickers, Selects } from "../../../component";
+import { Buttons, Selects } from "../../../component";
 import { FONTSTYLE } from "../../../component/font";
 import {
   FileExcelOutlined,
@@ -25,14 +22,10 @@ import { setGlobalTitle } from "../../../store/global";
 import {
   BASE_PATH_KARTU,
   BASE_PATH_PANUTAN,
-  EXPENDITURE_APP,
-  UNITPANUTAN,
   URL_PANUTAN,
   URL_SIAKUN,
 } from "../../../config/api";
 import axios from "axios";
-import ribuan from "../../../utils/formatribu";
-import { useNavigate } from "react-router-dom";
 import { formatDate } from "../../../utils/format_tgl_indo";
 import moment from "moment";
 import DokumenPdf from "./DokumenPdf";
@@ -40,14 +33,13 @@ import { formatRupiah } from "../../../utils/formatRP";
 import ExcelCreate from "./component/excelcreate";
 
 const BukuBantuBelanja = () => {
-  const { Search } = Input;
   const { Option } = Select;
   const [data, setData] = useState([]);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [dataUnit, setDataUnit] = useState(null);
-  const [selectedUnit, setSelectedUnit] = useState("");
+  const [selectedUnit, setSelectedUnit] = useState('');
+  const [dataCoa, setDataCoa] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [selectedOption, setSelectedOption] = useState(null);
   const [selectedDate, setSelectedDate] = useState(null);
@@ -55,20 +47,34 @@ const BukuBantuBelanja = () => {
   const [selectedYear, setSelectedYear] = useState(null);
   const [isEditForm, setIsEditForm] = useState(false);
   const [isEditDone, setIsEditDone] = useState(false);
-  const val = {};
-  console.log("data", selectedUnit);
-  const bulan = (new Date().getMonth() + 1).toString().padStart(2, "0");
+  const [searchText, setSearchText] = useState("");
+  const kode_unit = selectedUnit;
+
+
+  console.log("======================");
+  console.log("data unit", kode_unit);
+  console.log("data coa", dataCoa);
+  console.log("=====================");
+  
   // ==================================================== //
+  const bulan = (new Date().getMonth() + 1).toString().padStart(2, "0");
   const dateObj = new Date();
   const month = dateObj.getUTCMonth() + 1;
   const day = dateObj.getUTCDate();
   const year = dateObj.getUTCFullYear();
   const newdate = year + "-" + month + "-" + day;
   // ==================================================== //
+
   useEffect(() => {
     getUnit();
-    dispatch(setGlobalTitle("Buku Bantu"));
+    dispatch(setGlobalTitle("Daftar Buku Bantu"));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (selectedUnit) {
+      getCoA(selectedUnit);
+    }
+  }, [selectedUnit]);
 
   const getUnit = () => {
     axios
@@ -80,7 +86,7 @@ const BukuBantuBelanja = () => {
           setDataUnit(response);
         } else {
           notification.error({
-            description: "Data Klasifikasi Tidak di temukan",
+            description: "Data Unit Tidak di temukan",
             key: 12,
           });
         }
@@ -92,6 +98,36 @@ const BukuBantuBelanja = () => {
         });
       });
   };
+  
+  const getCoA = () => {
+    axios
+      .get(URL_SIAKUN + BASE_PATH_KARTU.get_coa + kode_unit)
+      .then((data) => {
+        const response = data.data.data;
+        console.log("coa", response);
+        if (response) {
+          if (response.length > 0) {
+            setDataCoa(response);
+          } else {
+            notification.error({
+              description: "Data CoA Tidak ditemukan",
+              key: 12,
+            });
+          }
+        } else {
+          notification.error({
+            description: "Data CoA Kosong atau tidak tersedia",
+            key: 13,
+          });
+        }
+      })
+      .catch((err) => {
+        notification.error({
+          description: err,
+          key: 14,
+        });
+      });
+  }
   const cekJadwal = async (val) => {
     const data = {
       tanggal_awal: val.tanggal_awal,
@@ -102,7 +138,7 @@ const BukuBantuBelanja = () => {
       akun_coa: val.akun_coa,
       kode_unit: val.kode_unit,
     };
-
+    console.log("data input", data);
     const o = {
       url: URL_SIAKUN + BASE_PATH_KARTU.list_kartu_nocontra,
       data: data,
@@ -119,14 +155,19 @@ const BukuBantuBelanja = () => {
       throw error;
     }
   };
-  // const pilunit = (value) => {
-  //   setDataUnit(value);
-  // };
+
   const pilunit = (value) => {
     setSelectedUnit(value);
+    setSelectedCategory(""); 
+    setSelectedOption(null); 
+    setSearchText("");
+    setData([]); 
   };
   const pilkategori = (value) => {
     setSelectedCategory(value);
+    setSelectedOption(null);
+    setSearchText("");
+    setData([]);
   };
 
   const onChangeOption = (e) => {
@@ -198,25 +239,20 @@ const BukuBantuBelanja = () => {
     }
 
     val.akun_coa = selectedCategory;
-    // val.kode_unit = dataUnit || "";
-    val.kode_unit = selectedUnit;
-
+    val.kode_unit = selectedUnit === "ALL" ? "" : selectedUnit;
     try {
       const data = await cekJadwal(val);
-      // Handle the returned data as needed
       setData(data.data);
       setIsEditForm(false);
       setIsEditDone(true);
       console.log(data);
     } catch (error) {
-      // Handle the error
       console.error(error);
     } finally {
-      // Set the loading state to false after the API request is completed
       setIsLoading(false);
     }
   };
-
+ 
   const colomntrans = [
     {
       title: "No",
@@ -229,11 +265,13 @@ const BukuBantuBelanja = () => {
         return <p className="m-0">{index + 1}</p>;
       },
     },
-    // {
-    //   title: "Unit",
-    //   dataIndex: "nama_unit",
-    //   key: "nama_unit",
-    // },
+    {
+      title: "Unit",
+      dataIndex: "nama_unit",
+      key: "nama_unit",
+      style: selectedUnit !== "ALL" ? {} : { display: "none" },
+    render: (text, record) => <div>{text}</div>,
+    },
     {
       title: "Tanggal",
       dataIndex: "tanggal_transaksi",
@@ -257,9 +295,9 @@ const BukuBantuBelanja = () => {
       width: 200,
       render: (text, record, index) => {
         if (text === "AKTIVA") {
-          return <div>{formatRupiah(record.nominal) + ",00"}</div>; // Display the "nominal" value if "penempatan" is "AKTIVA"
+          return <div>{formatRupiah(record.nominal) + ",00"}</div>;
         } else {
-          return ""; // Return null if "penempatan" is not "AKTIVA"
+          return ""; 
         }
       },
     },
@@ -271,13 +309,69 @@ const BukuBantuBelanja = () => {
       width: 200,
       render: (text, record, index) => {
         if (text === "PASIVA") {
-          return <div>{formatRupiah(record.nominal) + ",00"}</div>; // Display the "nominal" value if "penempatan" is "AKTIVA"
+          return <div>{formatRupiah(record.nominal) + ",00"}</div>; 
         } else {
-          return ""; // Return null if "penempatan" is not "AKTIVA"
+          return ""; 
         }
       },
     },
   ];
+  const colomn = [
+    {
+      title: "No",
+      dataIndex: "index",
+      key: "index",
+      width: 70,
+      render: (_, record, index) => {
+        return <p className="m-0">{index + 1}</p>;
+      },
+    },
+    {
+      title: "Tanggal",
+      dataIndex: "tanggal_transaksi",
+      key: "tanggal_transaksi",
+      width: 150,
+      sorter: (a, b) =>
+        new Date(a.tanggal_transaksi).getTime() -
+        new Date(b.tanggal_transaksi).getTime(),
+      render: (text, record) => <div>{formatDate(text)}</div>,
+    },
+    {
+      title: " Keterangan ",
+      dataIndex: "keterangan",
+      key: "keterangan",
+    },
+    {
+      title: "Debit",
+      dataIndex: "penempatan",
+      key: "Aktiva",
+      align: "right",
+      width: 200,
+      render: (text, record, index) => {
+        if (text === "AKTIVA") {
+          return <div>{formatRupiah(record.nominal) + ",00"}</div>;
+        } else {
+          return ""; 
+        }
+      },
+    },
+    {
+      title: "Kredit",
+      dataIndex: "penempatan",
+      key: "pasiva",
+      align: "right",
+      width: 200,
+      render: (text, record, index) => {
+        if (text === "PASIVA") {
+          return <div>{formatRupiah(record.nominal) + ",00"}</div>; 
+        } else {
+          return ""; 
+        }
+      },
+    },
+  ];
+
+  const columns = selectedUnit === "ALL" ? colomntrans : colomn;
   return (
     <div className="p-5 bg-white rounded-lg">
       <div
@@ -330,7 +424,6 @@ const BukuBantuBelanja = () => {
             icon={<ReloadOutlined />}
             onClick={handleResetClick}
           />
-          {/* Render your Ant Design table component */}
         </div>
       </div>
       <Divider />
@@ -354,7 +447,7 @@ const BukuBantuBelanja = () => {
             value={selectedUnit}
             optionContent={
               <>
-                <Option value="">---Silahkan Pilih Unit---</Option>
+                <Option value="ALL">---Semua Unit---</Option>
                 {Array.isArray(dataUnit) &&
                   dataUnit.map((item) =>
                     item.kode_unit !== null ? (
@@ -367,6 +460,7 @@ const BukuBantuBelanja = () => {
             }
           />
         </div>
+
         <div
           className="grid grid-cols-1"
           style={{ fontFamily: FONTSTYLE.POPPINS }}
@@ -375,25 +469,36 @@ const BukuBantuBelanja = () => {
             Pilih Kategori:
           </label>
           <Selects
-            style={{ height: 200 }}
-            marginBottom={10}
-            filterOption={(input, option) =>
-              String(option.value)
-                .toLowerCase()
-                .indexOf(input.toLowerCase()) !== -1 ||
-              option.children.toLowerCase().indexOf(input.toLowerCase()) !== -1
-            }
-            onChange={pilkategori}
-            placeholder={"---Silahkan Pilih Kategori---"}
-            optionContent={
-              <>
-                <Option value={110101010100}>Buku Bantu Kas</Option>
-                <Option value={5}>Buku Bantu Belanja</Option>
-                <Option value={2102}>Buku Bantu Pajak</Option>
-                <Option value={2}>Buku Bantu Hutang</Option>
-              </>
-            }
-          />
+              style={{ height: 200 }}
+              marginBottom={10}
+              filterOption={(input, option) => {
+                const optionValue = option.props.value || "";
+                const optionChildren = option.props.children || "";
+                const inputValue = input.toLowerCase();
+            
+                return (
+                  optionValue.toLowerCase().includes(inputValue) ||
+                  optionChildren.toString().toLowerCase().includes(inputValue)
+                );
+              }}
+              onChange={pilkategori}
+              value={selectedCategory}
+              placeholder={"---Silahkan Pilih Chart of Account (CoA)---"}
+              optionContent={
+                <>
+                  
+                  {Array.isArray(dataCoa) &&
+                    dataCoa.map((item) =>
+                      item.akun_coa !== null ? (
+                        <Option key={item.akun_coa} value={item.akun_coa}>
+                          {item.keterangan}&nbsp;{item.akun_coa}
+                        </Option>
+                      ) : null
+                    )}
+                </>
+              }
+              disabled={!selectedUnit}
+            />
         </div>
 
         <div
@@ -403,7 +508,7 @@ const BukuBantuBelanja = () => {
           <label className="block mb-1 text-md font-bold">
             Pilih Berdasarkan:
           </label>
-          <Radio.Group onChange={onChangeOption} value={selectedOption}>
+          <Radio.Group onChange={onChangeOption} value={selectedOption} disabled={!selectedCategory}>
             <Radio value={1}>Tanggal</Radio>
             <Radio value={2}>Bulan</Radio>
             <Radio value={3}>Tahun</Radio>
@@ -426,7 +531,7 @@ const BukuBantuBelanja = () => {
                 onChange={onChangeDate}
               />
               {isLoading ? (
-                <Spin size="large" /> // Render the loading animation while isLoading is true
+                <Spin size="large" /> 
               ) : (
                 <Buttons
                   labelButton={"Proses"}
@@ -458,17 +563,8 @@ const BukuBantuBelanja = () => {
                 onChange={onChangeMonth}
               />
 
-              {/* <Buttons
-                labelButton={"Proses"}
-                borderColor={"#229CE1"}
-                backgroundColor={"#229CE1"}
-                color={"white"}
-                marginLeft={5}
-                icon={<SyncOutlined />}
-                onClick={handleProsesClick}
-              /> */}
               {isLoading ? (
-                <Spin size="large" /> // Render the loading animation while isLoading is true
+                <Spin size="large" /> 
               ) : (
                 <Buttons
                   labelButton={"Proses"}
@@ -501,7 +597,7 @@ const BukuBantuBelanja = () => {
               />
 
               {isLoading ? (
-                <Spin size="large" /> // Render the loading animation while isLoading is true
+                <Spin size="large" /> 
               ) : (
                 <Buttons
                   labelButton={"Proses"}
@@ -518,8 +614,21 @@ const BukuBantuBelanja = () => {
             <p></p>
           )}
         </div>
+        <Divider/>
+        <div>
+          <label className="block mb-1 text-md font-bold">
+            Cari Data:
+          </label>
+          <Input
+            style={{ marginBottom: 16, marginTop:2 }}
+            placeholder="Search..."
+            value={searchText}
+            disabled={!data || data.length === 0}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
       </div>
-
+      </div>
+      
       <div
         style={{
           padding: 5,
@@ -532,8 +641,28 @@ const BukuBantuBelanja = () => {
         }}
       >
         <Table
-          dataSource={data}
-          columns={colomntrans}
+        
+          dataSource={data.filter((item) => {
+            const formattedSearchText = searchText.toLowerCase();
+    
+            // Filter based on searchText
+            const searchMatch = Object.values(item).some((value) => {
+              const formattedValue = value.toString().toLowerCase();
+              return formattedValue.includes(formattedSearchText);
+            });
+            if (searchMatch) return true;
+    
+            // Filter based on specific date
+            const searchDate = moment(searchText, "D MMMM", true);
+            if (searchDate.isValid()) {
+              const itemDate = moment(item.tanggal_transaksi, "YYYY-MM-DD");
+              return itemDate.format("D MMMM") === searchDate.format("D MMMM");
+            }
+    
+            return false;
+          })}
+          
+          columns={columns}
           bordered
           pagination={{ pageSize: 50 }}
           scroll={{ y: 2000 }}
